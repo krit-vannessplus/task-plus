9b383069bdccd729975c9b8fae7c6a6d3e36fa9c
Krit Hnoorun <krithnoorun@gmail.com>
2025-07-30 17:31:12 +0700

    add async to taskPage

--- app/task/[taskId]/page.tsx
+++ app/task/[taskId]/page.tsx
@@ -1,6 +1,10 @@
 import { TaskWrapper } from "@/components/taskWrapper";
 
-export default function TaskPage({ params }: { params: { taskId: string } }) {
+export default async function TaskPage({
+  params,
+}: {
+  params: { taskId: string };
+}) {
   const { taskId } = params;
   console.log("Task ID:", taskId); // Debugging line to check taskId
   return <TaskWrapper taskId={taskId} />;
