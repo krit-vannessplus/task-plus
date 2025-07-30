f239de8d4590e9173f243c737d1d1bd4d6807a60
Krit Hnoorun <krithnoorun@gmail.com>
2025-07-30 17:38:49 +0700

    fix the param usage

--- app/task/[taskId]/page.tsx
+++ app/task/[taskId]/page.tsx
@@ -1,10 +1,11 @@
 import { TaskWrapper } from "@/components/taskWrapper";
 
-export default async function TaskPage({
-  params,
-}: {
+type Props = {
   params: { taskId: string };
-}) {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default async function TaskPage({ params }: Props) {
   const { taskId } = params;
   console.log("Task ID:", taskId); // Debugging line to check taskId
   return <TaskWrapper taskId={taskId} />;
